ff97552d0e8146677767b065c496284c2913fa71
austinbarrows <austinm.barrows@gmail.com>
2020-10-10 21:24:17 -0400

    Finish data allocation method

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-
 let deathRates = {
     //https://www.acsh.org/news/2020/06/23/coronavirus-covid-deaths-us-age-race-14863
     //https://osf.io/wdbpe/
     // Population
-    CHILD_DEATH: 0.000016,
-    TEEN_DEATH: 0.0000032,
-    ADULT_DEATH_20_49: 0.000092,
-    ADULT_DEATH_50_64: 0.0014,
-    ADULT_DEATH_65UP: 0.056,
+    CHILD: 0.000016,
+    TEEN: 0.0000032,
+    ADULT_20_49: 0.000092,
+    ADULT_50_64: 0.0014,
+    ADULT_65UP: 0.056,
 };
 
 let reducers = {
@@ -60,17 +59,18 @@ let selectorIncreasers = {
     LOCAL_TRAVEL: 1
 }
 
-let deathTotal = [];
+let deathsTotal = [];
 let deathsChild = [];
 let deathsTeen = [];
 let deaths20_49 = [];
 let deaths50_64 = [];
-let deaths65up = [];function generateData() {
+let deaths65up = [];
+
+function generateData(days) {
     let cases = 10;
     let base = 1.1;
 
-    let increasers = 0;
-    let expFactor = base; // Multiply by all reducers and increasers
+    let expFactor = base; // Start with base and multiply by all reducers and increasers
 
     Object.keys(reducers).forEach(function(factor) {
         expFactor = expFactor * (1 - (reducers[factor] * selectorReducers[factor]));
@@ -80,7 +80,18 @@ let deaths65up = [];function generateData() {
         expFactor = expFactor * (1 + (increasers[factor] * selectorIncreasers[factor]));
     });
 
-    
+    // Use expFactor to simulate the spread of disease (which usually follows an exponential function) for a number of days
+    for(let i = 0; i < days; i++) {
+        // Increase number of cases accordingly
+        cases = Math.ceil(cases * expFactor);
+        // Increase number of deaths according to the number of cases, not exactly accurate to reality since there is a 2 week delay *****MIGHT FIX LATER
+        deathsChild[i] = Math.floor(deathRates[CHILD] * cases);
+        deathsTeen[i] = Math.floor(deathRates[TEEN] * cases);
+        deaths20_49[i] = Math.floor(deathRates[ADULT_20_49] * cases);
+        deaths50_64[i] = Math.floor(deathRates[ADULT_50_64] * cases);
+        deaths65up[i] = Math.floor(deathRates[ADULT_65UP] * cases);
+        deathsTotal[i] = deathsChild[i] + deathsTeen[i] + deaths20_49[i] + deaths50_64[i] + deaths65up[i];
+    }
 
 }
 
